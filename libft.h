37f5cfbba482ff47614d1ce857f828511e241739
Noshin Yeasmin <noshin.yeasmin@Noshins-Air.fritz.box>
2025-07-16 16:10:05 +0200

    libft.h added and rest of the ft modified

---
+++ libft.h
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mdchowdh <mdchowdh@student.42heilbronn.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/16 14:05:01 by mdchowdh          #+#    #+#             */
+/*   Updated: 2025/07/16 16:05:06 by mdchowdh         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+#define LIBFT_H
+
+#include <stddef.h>
+
+// Function prototypes
+int ft_isalpha(int c);
+int	ft_isdigit(int c);
+int	ft_isalnum(int c);
+int	ft_isascii(int c);
+int	ft_isprint(int c);
+int	ft_strlen(char *str);
+void	*ft_memset(void *b, int c, size_t len);
+void    ft_bzero(void *s, size_t n);
+void	*ft_memcpy(void *dst, const void *src, size_t n);
+void	*ft_memmove(void *dst, const void *src, size_t n);
+
+#endif
