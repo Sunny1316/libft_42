6f12c73406ac774dd55061092b2d9a8fc6dd158c
Md Sunyuzzaman Chowdhury <mdchowdh@1-C-15.42heilbronn.de>
2025-07-20 20:58:32 +0200

    modified ft_strrchr.c

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,43 +6,28 @@
 /*   By: mdchowdh <mdchowdh@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/17 14:03:08 by mdchowdh          #+#    #+#             */
-/*   Updated: 2025/07/20 18:13:12 by mdchowdh         ###   ########.fr       */
+/*   Updated: 2025/07/20 20:56:34 by mdchowdh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
 char	*ft_strrchr(const char *s, int c)
+
 {
 	int	i;
 
+	i = ft_strlen(s);
 	if (!s)
 		return (NULL);
-	i = ft_strlen(s);
 	while (i >= 0)
 	{
 		if (s[i] == (unsigned char)c)
-			return ((char *)&s[i]);
-		if (i == 0)
-			break;
+			return ((char *)s + i);
 		i--;
 	}
 	return (NULL);
 }
-/* {
-	int	i;
-
-	if (!s)
-		return (NULL);
-	i = ft_strlen(s);
-	while (i >= 0)
-	{
-		if (s[i] == (unsigned char)c)
-			return ((char *)&s[i]);
-		i--;
-	}
-	return (NULL);
-} */
 /* #include <stdio.h>
 #include <string.h>
 int main(void)
@@ -53,9 +38,9 @@ int main(void)
 
     if (my_result && lib_result)
        {
-        printf("my_result:Found last 'o' at : %p\n", my_result); 
-        printf("lib_result:Found last 'o' at : %p\n", lib_result); 
-       }  
+        printf("my_result:Found last 'o' at : %p\n", my_result);
+        printf("lib_result:Found last 'o' at : %p\n", lib_result);
+       }
     else if(!my_result && !lib_result)
         {
             printf("my_result:'z' not found. %s\n", my_result);
