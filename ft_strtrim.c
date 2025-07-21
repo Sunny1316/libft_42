a9ded04520536da84494c4d2900f0009f03a3f5c
Md Sunyuzzaman Chowdhury <mdchowdh@1-C-7.42heilbronn.de>
2025-07-21 16:20:20 +0200

    main function added

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: mdchowdh <mdchowdh@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/20 14:35:26 by mdchowdh          #+#    #+#             */
-/*   Updated: 2025/07/20 14:43:08 by mdchowdh         ###   ########.fr       */
+/*   Updated: 2025/07/21 16:19:52 by mdchowdh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,3 +43,15 @@ char	*ft_strtrim(char const *s1, char const *set)
 	ft_strlcpy(trimmed, s1 + start, end - start + 1);
 	return (trimmed);
 }
+
+/*  #include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+	char *str = "    Hello, world!   ";
+   char *newStr = ft_strtrim(str, " ");
+   printf("original string : %s\n", str);
+   printf("trimmed string : %s\n", newStr);
+   return(0);
+} */
